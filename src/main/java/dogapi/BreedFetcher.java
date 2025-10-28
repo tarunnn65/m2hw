0ede6657088c8281b0cff5fefce785194ecc65dd
tarunnn65 <kstarunraj@gmail.com>
2025-10-27 23:44:34 -0400

    Completed Task 3 and 4

--- src/main/java/dogapi/BreedFetcher.java
+++ src/main/java/dogapi/BreedFetcher.java
@@ -9,6 +9,7 @@ public interface BreedFetcher {
 
     /**
      * Fetch the list of sub breeds for the given breed.
+     *
      * @param breed the breed to fetch sub breeds for
      * @return list of sub breeds for the given breed
      * @throws BreedNotFoundException if the breed does not exist
@@ -18,9 +19,9 @@ public interface BreedFetcher {
 
     // TODO Task 4: make this a checked exception and update any other code as needed.
     // a class defined in an interface is public AND static
-    class BreedNotFoundException extends RuntimeException {
+    class BreedNotFoundException extends Exception {
         public BreedNotFoundException(String breed) {
-            super("Breed not found: " + breed);
+            super("Breed not found: " + breed);//completed all tasks
         }
     }
 }
